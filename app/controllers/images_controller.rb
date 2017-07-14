b804a584cbcb179ef9dac529cc6ab69b6602a293
Sakis Karagiannis <sakiskar90@gmail.com>
2017-07-14 14:11:29 +0100

    ADD: Upload form

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,12 @@
 class ImagesController < ApplicationController
   def upload
   end
+
+  def perform_upload
+    if params[:images][:csv].content_type != 'text/csv'
+      flash[:error] = 'Uploaded file not csv'
+    end
+    flash[:success] = 'Correct file'
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'images/upload'
+  post 'images/perform_upload'
   root 'images#upload'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
