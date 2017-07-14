b62edba9eb303bd9b83d5e9e10c4a3a2da7b1ffd
Sakis Karagiannis <sakiskar90@gmail.com>
2017-07-14 19:14:49 +0100

    ADD: Initial specs

--- spec/factories/montages.rb
+++ spec/factories/montages.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
   factory :montage do
-    left_image "MyString"
-    right_image "MyString"
-    combined_image "MyString"
+    left_image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'support', 'images', 'image1.jpg'), 'image/jpg') }
+    right_image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'support', 'images', 'image2.jpg'), 'image/jpg') }
   end
 end
--- spec/models/montage_spec.rb
+++ spec/models/montage_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe Montage, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'Montage' do
+
+  # describe 'test' do
+  #   before do
+  #     @test = build :montage
+  #     @test.save!
+  #   end
+  #
+  #   it 'should work' do
+  #     expect(@test).to validate_presence_of :left_image
+  #   end
+  # end
+
+  subject { build_stubbed :montage }
+
+  it { is_expected.to validate_presence_of :left_image }
+  it { is_expected.to validate_presence_of :right_image }
 end
