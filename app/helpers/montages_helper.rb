7ffe3e241f06a07f0dcf1d51cca10ec66b466ff1
Sakis Karagiannis <sakiskar90@gmail.com>
2017-07-15 13:04:40 +0100

    CHG: Extract create_montage method

--- app/helpers/montages_helper.rb
+++ app/helpers/montages_helper.rb
@@ -4,10 +4,14 @@ require 'open-uri'
 module MontagesHelper
   def import_csv(csv)
     CSV.foreach(csv.path) do |row|
-      m = Montage.new
-      m.left_image = open(row[0])
-      m.right_image = open(row[1])
-      m.save
+      create_montage(row)
     end
+  end
+
+  def create_montage(row)
+    m = Montage.new
+    m.left_image = open(row[0])
+    m.right_image = open(row[1])
+    m.save # Silent error handling to allow idempotent uploads
   end
 end
