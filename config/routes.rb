ebb86773efe47c0a4d3f904ebe2bcc263aa4dd37
Sakis Karagiannis <sakiskar90@gmail.com>
2017-07-16 13:49:53 +0100

    ADD: Sidekiq web ui

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get 'images/download' => 'images#download'
   root 'images#upload'
 
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
